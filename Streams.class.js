b146a241df09ce066da2bc96be5604c78aaedb28
Chateauroux Guillaume <guillaume.chateauroux@gmail.com>
2020-06-01 06:51:10 +0200

    fix possible invalid access token

--- Streams.class.js
+++ Streams.class.js
@@ -16,15 +16,23 @@ class Streams {
         this.canFetch = true;
 
         axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`)
-            .then(r => {
-                this.bearer = r.data.access_token;
+            .then(({data}) => {
+                this.bearer = data.access_token;
                 this.startFetchStreams();
+                setTimeout(() => this.refreshToken(), (data.expires_in * 60));
             });
 
         this.events = {
-            'fetchFinished': () => {
-            },
+            'fetchFinished': () => ({}),
         };
+    }
+
+    refreshToken() {
+        axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`)
+            .then(({data}) => {
+                this.bearer = data.access_token;
+                setTimeout(() => this.refreshToken(), (data.expires_in * 60));
+            });
     }
 
     /// GETTER FUNCTIONS
